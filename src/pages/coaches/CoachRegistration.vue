3aad7c6098a69a4007d05b8bc32f054c45fb1cca
wocjechdominiak <wojciech.dominiak96@gmail.com>
2022-05-09 18:52:53 +0200

    add rregistration form

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,19 @@
 <template>
-    REGISTER
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import BaseCard from "@/components/ui/BaseCard.vue";
+import CoachForm from "../../components/coaches/CoachForm.vue";
+export default {
+  components: {
+    CoachForm,
+    BaseCard,
+  },
+};
+</script>
