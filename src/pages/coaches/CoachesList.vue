b6ceee5420bdabaea95f9d5a1f69be5662e7a993
wocjechdominiak <wojciech.dominiak96@gmail.com>
2022-05-11 18:25:56 +0200

    added error handler

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,4 +1,7 @@
 <template>
+  <base-dialog :show="!!error" title="An error occured!" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
   <coach-filter @change-filter="setFilters"></coach-filter>
   <section>
     <base-card>
@@ -35,6 +38,7 @@ export default {
   data() {
     return {
       isLoading: false,
+      error: null,
       activeFilter: {
         frontend: true,
         backend: true,
@@ -71,8 +75,15 @@ export default {
   methods: {
     async loadCoaches() {
       this.isLoading = true;
-      await this.$store.dispatch("coaches/loadCoaches");
+      try {
+        await this.$store.dispatch("coaches/loadCoaches");
+      } catch (error) {
+        this.error = error.message || "Something went wrong";
+      }
       this.isLoading = false;
+    },
+    handleError(){
+      this.error = null;
     },
     setFilters(updatedFilters) {
       this.activeFilter = updatedFilters;
