dbe312235bd92f1c1123eeed7353d7a2e5222dfd
wocjechdominiak <wojciech.dominiak96@gmail.com>
2022-05-12 16:14:59 +0200

    added HTTP Requests

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,10 +1,14 @@
 <template>
+  <base-dialog :show="!!error" title="An error occured!" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <base-card>
       <header>
         <h2>Requests Received</h2>
       </header>
-      <ul v-if="hasRequests">
+      <base-spinner v-if="isLoading"></base-spinner>
+      <ul v-else-if="hasRequests && !isLoading">
         <request-item
           v-for="req in reveivedRequests"
           :key="req.id"
@@ -21,12 +25,35 @@
 import RequestItem from "../../components/requests/RequestItem.vue";
 export default {
   components: { RequestItem },
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+    };
+  },
+  created() {
+    this.loadRequests();
+  },
   computed: {
     reveivedRequests() {
       return this.$store.getters["requests/requests"];
     },
     hasRequests() {
       return this.$store.getters["requests/hasRequests"];
+    },
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch["requests/fetchRequests"];
+      } catch (error) {
+        this.error = error.message || "Something went wrong!";
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
     },
   },
 };
--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,54 @@
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
-      id: new Date().toISOString(),
-      coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message,
     };
+    const response = await fetch(
+      `https://vue-http-demo-e5d5c-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`,
+      {
+        method: "POST",
+        body: JSON.stringify(newRequest),
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || "Failed to send request");
+      throw error;
+    }
+
+    newRequest.id = responseData.name;
+    newRequest.coachId = payload.coachId;
+
     context.commit("addRequest", newRequest);
+  },
+  async fetchRequestes(context) {
+    const coachId = context.rootGetters.userId;
+    console.log(coachId);
+    const response = await fetch(
+      `https://vue-http-demo-e5d5c-default-rtdb.firebaseio.com/requests/${coachId}.json`
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || "Failed to fetch request"
+      );
+      throw error;
+    }
+    const requests = [];
+    for (const key in responseData) {
+      const request = {
+        id: key,
+        coachId: coachId,
+        userEmail: responseData[key].userEmail,
+        message: responseData[key].message,
+      };
+      requests.push(request);
+    }
+    context.commit("setRequests", requests);
   },
 };
