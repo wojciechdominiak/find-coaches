c41fec47f2526f6e87ad113aab9c44b10be5400b
wocjechdominiak <wojciech.dominiak96@gmail.com>
2022-05-13 15:21:45 +0200

    added autologout

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,23 @@ export default {
   components: {
     TheHeader,
   },
+  computed: {
+    didAutoLogout() {
+      return this.$store.getters.didAutoLogout;
+    }
+  },
   created() {
     this.$store.dispatch("tryLogin");
   },
+  watch: {
+    didAutoLogout(curValue, oldValue) {
+        if (curValue && curValue !== oldValue){
+          this.$router.replace('/coaches');
+        }
+    }
+  }
 };
 </script>
-
-<script></script>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
