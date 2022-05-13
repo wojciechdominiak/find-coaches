c1f23930888d67f49a58253ffea5c6d045c45f51
wocjechdominiak <wojciech.dominiak96@gmail.com>
2022-05-13 13:45:48 +0200

    add navigation guard

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -83,6 +83,8 @@ export default {
             password: this.password,
           });
         }
+        const redirectUrl = "/" + (this.$route.query.redirect || "coaches");
+        this.$router.replace(redirectUrl);
       } catch (err) {
         this.error = err.message || "Failed to authenticate, try later";
       }
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -10,7 +10,9 @@
           <base-button mode="outline" @click="loadCoaches(true)"
             >Refresh</base-button
           >
-          <base-button link to="/auth" v-if="!isLoggedIn">Login</base-button>
+          <base-button link to="/auth?redirect=register" v-if="!isLoggedIn"
+            >Login to Register as a coach</base-button
+          >
           <base-button
             v-if="isLoggedIn && !isCoach && !isLoading"
             link
