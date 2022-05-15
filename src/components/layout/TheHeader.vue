165cd35854423a2da390417b652954a26728affa
wocjechdominiak <wojciech.dominiak96@gmail.com>
2022-05-15 17:17:57 +0200

    header done

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,19 +1,48 @@
 <template>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
+  />
   <header>
     <nav>
       <h1>
-        <router-link to="/find-coaches">Find a <span>Coach</span></router-link>
+        <router-link @click="disActive" to="/find-coaches"
+          >Find a <span>Coach</span></router-link
+        >
       </h1>
-      <ul>
+      <button @click="toggleActive" class="nav-header__toggle">
+        <i class="fas fa-bars"></i>
+      </button>
+      <ul :class="{ inactive: !isActive }">
         <li>
-          <router-link class="link" to="/find-coaches/coaches">All coaches</router-link>
+          <router-link
+            @click="disActive"
+            class="link"
+            to="/find-coaches/coaches"
+            >All coaches</router-link
+          >
         </li>
         <li v-if="isLoggedIn">
-          <router-link class="link" to="/find-coaches/requests">Requestes</router-link>
+          <router-link
+            @click="disActive"
+            class="link"
+            to="/find-coaches/requests"
+            >Requestes</router-link
+          >
         </li>
-        <li v-else><router-link class="link" to="/find-coaches/auth">Login</router-link></li>
+        <li v-else>
+          <router-link @click="disActive" class="link" to="/find-coaches/auth"
+            >Login</router-link
+          >
+        </li>
         <li>
-          <base-button @click="logout" v-if="isLoggedIn">Logout</base-button>
+          <router-link
+            class="link"
+            to="/find-coaches/auth"
+            @click="logout"
+            v-if="isLoggedIn"
+            >Logout</router-link
+          >
         </li>
       </ul>
     </nav>
@@ -21,9 +50,12 @@
 </template>
 
 <script>
-import BaseButton from "../ui/BaseButton.vue";
 export default {
-  components: { BaseButton },
+  data() {
+    return {
+      isActive: false,
+    };
+  },
   computed: {
     isLoggedIn() {
       return this.$store.getters.isAuthenticated;
@@ -33,6 +65,13 @@ export default {
     logout() {
       this.$store.dispatch("logout");
       this.$router.replace("/find-coaches/coaches");
+      this.isActive = false;
+    },
+    toggleActive() {
+      this.isActive = !this.isActive;
+    },
+    disActive() {
+      this.isActive = false;
     },
   },
 };
@@ -41,67 +80,97 @@ export default {
 <style scoped>
 header {
   width: 100%;
-  height: 6rem;
+  height: 5rem;
   background-color: #3d008d;
   box-shadow: 0 1px 4px #3d008d;
+  display: grid;
+  place-items: center;
+}
+
+header nav {
+  width: 80%;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
 }
 
-header a {
-  font-weight: bold;
-  text-decoration: none;
-  color: rgb(255, 252, 252);
-  display: inline-block;
-  padding: 0.75rem 1.25rem;
-  border: 1px solid transparent;
-}
-
-.link:active,
-.link:hover,
-.link.router-link-active {
-  border-radius: 1.5rem;
-  background-color: rgb(255, 252, 252);
-  color: #3d008d;
-}
-
 h1 a {
-  background-color: #3d008d;
+  text-decoration: none;
   color: white;
   margin: 0;
-  font-size: 1.7rem;
-}
-
-h1 a:active,
-h1 a:hover,
-h1 a.router-link-active {
-  background-color: #3d008d;
-  color: rgb(255, 252, 252);
+  font-size: 1.5rem;
 }
 
 span {
   color: rgb(65, 142, 155);
 }
 
-header nav {
-  width: 90%;
-  margin: auto;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+.nav-header__toggle {
+  font-size: 1.5rem;
+  color: white;
+  background: transparent;
+  border-color: transparent;
+  transition: 0.3s;
+  cursor: pointer;
+}
+
+.nav-header__toggle:hover {
+  color: rgb(65, 142, 155);
 }
 
 header ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
+  position: absolute;
+  top: 5rem;
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+  left: 0;
+  list-style: none;
+  margin: 0;
+  padding:0;
+  color: rgb(255, 252, 252);
+  background-color: #3d008d;
+  width: 100%;
+}
+
+.inactive {
+  display: none;
 }
 
 li {
-  margin: 0 0.5rem;
+  margin: 0 .5rem;
+}
+
+.link {
+  font-weight: bold;
+  font-size: 1.2rem;
+  text-decoration: none;
+  color: rgb(255, 252, 252);
+  background-color: #3d008d;
+  display: inline-block;
+  padding: 0.75rem 1.25rem;
+}
+
+.link:active,
+.link:hover,
+.link.router-link-active {
+  color: rgb(65, 142, 155);
+  background-color: #3d008d;
+}
+
+@media screen and (min-width: 768px) {
+  .inactive,
+  header ul {
+    position: static;
+    list-style: none;
+    display: flex;
+    justify-content: right;
+    flex-direction: row;
+    width: auto;
+  }
+  .nav-header__toggle {
+    display: none;
+  }
 }
 </style>
